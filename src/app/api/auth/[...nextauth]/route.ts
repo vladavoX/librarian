c2e5a7264b63555b323a1091d2d2bceb84854a99
Vladimir Aleksic <vlada.pzs@hotmail.com>
2024-07-05 13:45:52 +0200

    Login and Register user

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,34 +1,4 @@
+import { authOptions } from '@/lib/auth'
 import NextAuth from 'next-auth'
-import Credentials from 'next-auth/providers/credentials'
-
-const handler = NextAuth({
-	providers: [
-		Credentials({
-			name: 'Credentials',
-			// `credentials` is used to generate a form on the sign in page.
-			// You can specify which fields should be submitted, by adding keys to the `credentials` object.
-			// e.g. domain, username, password, 2FA token, etc.
-			// You can pass any HTML attribute to the <input> tag through the object.
-			credentials: {
-				email: {},
-				password: {}
-			},
-			async authorize(credentials, req) {
-				// Add logic here to look up the user from the credentials supplied
-				const user = { id: '1', name: 'J Smith', email: 'jsmith@example.com' }
-
-				if (user) {
-					// Any object returned will be saved in `user` property of the JWT
-					return user
-				} else {
-					// If you return null then an error will be displayed advising the user to check their details.
-					return null
-
-					// You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
-				}
-			}
-		})
-	]
-})
-
+const handler = NextAuth(authOptions)
 export { handler as GET, handler as POST }
--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -1,28 +1,14 @@
-import { MongoClient } from 'mongodb'
+import mongoose from 'mongoose'
+const { MONGODB_URI } = process.env
 
-if (!process.env.MONGODB_URI) {
-	throw new Error('Invalid/Missing environment variable: "MONGODB_URI"')
-}
-
-const uri = process.env.MONGODB_URI
-const options = {}
-
-let client: MongoClient
-
-if (process.env.NODE_ENV === 'development') {
-	// In development mode, use a global variable so that the value
-	// is preserved across module reloads caused by HMR (Hot Module Replacement).
-	let globalWithMongo = global as typeof globalThis & {
-		_mongoClient?: MongoClient
+export const connectDB = async () => {
+	try {
+		const { connection } = await mongoose.connect(MONGODB_URI as string)
+		if (connection.readyState === 1) {
+			return Promise.resolve(true)
+		}
+	} catch (error) {
+		console.error(error)
+		return Promise.reject(error)
 	}
-
-	if (!globalWithMongo._mongoClient) {
-		globalWithMongo._mongoClient = new MongoClient(uri, options)
-	}
-	client = globalWithMongo._mongoClient
-} else {
-	// In production mode, it's best to not use a global variable.
-	client = new MongoClient(uri, options)
 }
-
-export default client
